d01994a03e236a6846123bf0189c4bdd7e12ea61
tombenamato <tombenamato@gmail.com>
2023-01-08 21:44:12 +0100

    decompressed better

--- data_downloader/processor.py
+++ data_downloader/processor.py
@@ -1,26 +1,8 @@
+import lzma
 import struct
 from lzma import LZMADecompressor, LZMAError, FORMAT_AUTO
 import polars as pl
-
-def decompress_lzma(data):
-    results = []
-    len(data)
-    while True:
-        decomp = LZMADecompressor(FORMAT_AUTO, None, None)
-        try:
-            res = decomp.decompress(data)
-        except LZMAError:
-            if results:
-                break  # Leftover data is not a valid LZMA/XZ stream; ignore it.
-            else:
-                raise  # Error on the first iteration; bail out.
-        results.append(res)
-        data = decomp.unused_data
-        if not data:
-            break
-        if not decomp.eof:
-            raise LZMAError("Compressed data ended before the end-of-stream marker was reached")
-    return b"".join(results)
+import numpy as np
 
 
 def tokenize(buffer):
@@ -34,15 +16,9 @@ def normalize(symbol, day, ticks):
                              ,pl.col("ask")/point, pl.col("bid")/point])
     df.drop_in_place("time")
     return df
-    #return df.to_dict(as_series=False)
-    #def norm(time, ask, bid, volume_ask, volume_bid):
-    #    date = datetime(day.year, day.month, day.day) + timedelta(milliseconds=time)
-    #    #date.replace(tzinfo=datetime.tzinfo("UTC"))
-    #    return date, ask / point, bid / point, volume_ask , volume_bid
-    #return map(lambda x: norm(*x), ticks)
 
 
 def decompress(symbol, day, compressed_buffer):
     if compressed_buffer.nbytes == 0:
         return compressed_buffer
-    return normalize(symbol, day, tokenize(decompress_lzma(compressed_buffer)))
+    return normalize(symbol, day, tokenize(lzma.decompress(compressed_buffer)))
