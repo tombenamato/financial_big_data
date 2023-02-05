AO: bug fix, writing and loading of average oracle eigen values

average_oracle/AO.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 
 
@@ -26,7 +25,8 @@ class AO():
         Args:
            AO_values_path (str): The path to the csv file where the average oracles values are
         """
-        self.AO_values = pd.read_csv(AO_values_path)
+        self.AO_values = np.genfromtxt(AO_values_path, delimiter=',')
+
 
     def filter_correlation_AO(self, C: np.ndarray) -> np.ndarray:
         """
@@ -37,7 +37,7 @@ class AO():
             ndarray : The filtered correlation matrix.
         """
         l, v = get_sortest_eig(C)
-        C_AO = (v @ AO @ v.T)
+        C_AO = (v @ np.diag(self.AO_values) @ v.T)
 
         return C_AO
 
@@ -45,14 +45,14 @@ class AO():
         """
         Filter the covariance matrix given and return it.
         Args:
-           Sigma (ndarray): The correlation to filter.
+           Sigma (ndarray): The covariance to filter.
         Returns:
-            ndarray : The filtered correlation matrix.
+            ndarray : The filtered covariance matrix.
         """
         s = np.sqrt(np.diag(Sigma))
         si_sj = np.outer(s, s)
         C = Sigma / si_sj
-        C_AO = self.filterCorrelation_AO(C)
+        C_AO = self.filter_correlation_AO(C)
 
         Sigma_AO = C_AO * si_sj
 